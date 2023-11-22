95e5ca983d22ae5b9378d9e0fbfb224111680bcb
Atoti2 <bogyikormi3@gmail.com>
2023-11-22 13:59:43 +0100

    dialog animation

--- tailwind_config.js
+++ tailwind_config.js
@@ -2,6 +2,20 @@ tailwind.config = {
     darkMode: 'class',
       theme: {
         extend: {
+          animation: {
+            modalf: "modalf 0.30s ease-in-out",
+            modalc: "modalc 0.30s ease-in-out",
+        },
+        keyframes: {
+            modalf: {
+                "0%": { transform: "scale(0)" },
+                "100%": { transform: "scale(1)" },
+            },
+            modalc: {
+              "0%": { transform: "scale(1)" },
+              "100%": { transform: "scale(0)" },
+            }
+        },
         }
       }
     }
